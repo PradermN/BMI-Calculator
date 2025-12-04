ccbb5ba8d19efaf26e11ea37496603b13b081d8a
PradermN <pradermnafack@gmail.com>
2025-12-04 23:27:02 +0100

    put widgets in new files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -53,53 +55,6 @@ class InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  final String label;
-  final IconData icon;
-  const IconContent({super.key, required this.label, required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-          color: Colors.white,
-        ),
-        SizedBox(height: 15.0),
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color color;
-  final Widget? cardChild;
-  const ReusableCard({super.key, required this.color, this.cardChild});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      height: 200.0,
-      child: cardChild,
     );
   }
 }
